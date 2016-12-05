37ea930b458ce3be3a74c512a725caf0b49b6316
Noah Wolfe <Noah Wolfe>
2016-12-04 23:48:44 -0500

    Some tests...

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    var apiKey = "AIzaSyCfcFfHlfdZrN16WdFcfayVC37yi-FVMo8";
+    var searchEngineId = "006378823057241998602:z4tzem8bwja";
+    var queryString = "2nd Great Awakening"
+    var restString = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${searchEngineId}&q=${queryString}`
+
+    var stringy = "https://en.wikipedia.org/wiki/Second_Great_Awakening";
+    request(stringy, function(err, gRes, body) {
+        res.render('index', { title: 'Express', results: body });
+    });
+
+    /*request(restString, function(err, gRes, body) {
+        if(!err && gRes.statusCode == 200) {
+            var parsedBody = JSON.parse(body);
+            console.dir(parsedBody);
+            var firstLink = parsedBody['items'][0]['link'];
+            console.log(firstLink);
+
+            request(firstLink, function(err, fRes, body) {
+                if(!err && fRes.statusCode == 200) {
+                    console.log(body);
+                }
+            });
+            // console.log(body);
+            // res.render('index', { title: 'Express', results: body });
+
+        } else {
+            console.error(err);
+            console.render('error', { error: err });
+        }
+    });*/
 });
 
 module.exports = router;
